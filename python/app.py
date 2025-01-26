e04cb9505a68023e7c6db67cc22c25b2ecd6900b
wodge73 <warren.legg@gmail.com>
2025-01-26 14:14:18 +0000

    wibble3

--- python/app.py
+++ python/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello Spuddles wibble2!"
+    return "Hello Spuddles wibble3!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
